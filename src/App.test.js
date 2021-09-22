a9fa306c60b7333217ce40393482abcd9cf274e0
danthb <danthb@hotmail.com>
2021-09-21 23:21:37 -0500

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,31 +1,7 @@
-import App from './App'
-import React from 'react';
-import {describe, expect, test} from '@jest/globals'
-import Enzyme from 'enzyme';
-import { shallow } from 'enzyme';
-import userEvent from '@testing-library/user-event'
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import {screen, render, debug} from '@testing-library/react'
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-import axios from './axios'
+test('renders learn react link', () => {
+  render(<App />);
 
-jest.disableAutomock();
-
-Enzyme.configure({ adapter: new Adapter() });
-jest.mock('axios')
-
-describe('Just for fun', () => {
-  test('renders learn react link', async () => {
-    const getSpy = jest.spyOn(axios, 'get');
-      const productsInstance = render(
-        <App/>
-      );
-      const button = productsInstance.getByText('ReStock Products')
-      userEvent.click(button)
-      expect(getSpy).toBeCalled();
-    
-  });
-
-  
-    
-})
+});
